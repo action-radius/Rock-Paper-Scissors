96dceffd2b691444adecf0278d7de2a45030b63f
action-radius <art.fedotov.ua@gmail.com>
2022-08-03 13:33:42 +0200

    x

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
+import random
 
 kivy.require('2.1.0')
 
+class MyRoot(BoxLayout):
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def generate_number(self):
+        self.random_label.text = str(random.randint(1, 1000))
+
 class NeuralRandom(App):
     def build(self):
-        return BoxLayout()
+        return MyRoot()
 
 neuralRandom = NeuralRandom()
 neuralRandom.run()
